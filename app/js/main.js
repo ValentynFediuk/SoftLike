09213531e78c3c05877add52a7908c92b6d64580
Valentyn <42549484+ValentynFediuk@users.noreply.github.com>
2021-12-11 18:07:40 +0200

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function () {
-  $('body').css('background-color', 'blue')
+  let typed = new Typed('.header__descr--typed', {
+    strings: ['Raise the next generation of leaders', 'Duplicate your success online', 'Make the best distributors', 'Maximize your business'],
+  smartBackspace: true, // Default value
+  loop: true,
+  typeSpeed: 80,
+  backSpeed: 40
+});
+
+  let menuBtn = document.querySelector('.menu__btn');
+  let menu = document.querySelector('.menu');
+
+  menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+  })
 })
